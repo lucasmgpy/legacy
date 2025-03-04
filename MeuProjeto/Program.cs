ab05b19ea798c506ed1f0413b0855fc52936aa01
lucas gesso <lucasmg.py@gmail.com>
2025-03-04 23:51:52 +0000

    connectionstring em programcs

--- MeuProjeto/Program.cs
+++ MeuProjeto/Program.cs
@@ -5,7 +5,7 @@ using MeuProjeto.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=/app/app.db";
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
